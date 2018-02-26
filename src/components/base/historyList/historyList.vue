12afea5f85bcc73a1af233961d95ece8cfd88444
shingli <745573545@qq.com>
2018-02-26 18:18:38 +0800

    throttle

---
+++ src/components/base/historyList/historyList.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="history-list-wrap">
+		<div class="has-history" v-if='hasMore'>
+			<ul>
+				<li>
+					<span>红海行动</span>
+					<div class="delete">
+						<i class="icon iconfont icon-delete"></i>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div class="nomore-history" v-else>
+			<span>还没有搜索记录，赶快去搜索吧！:)</span>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name:'historyList',
+		props:{
+			hasMore:{
+				type:Boolean,
+				default:true
+			}
+		}
+	}
+</script>
+<style scoped lang='less'>
+	@import '../../../assets/less/color.less';
+	.history-list-wrap{
+		.has-history{
+			li{
+				display: flex;
+				justify-content: space-between;
+				font-size: @size-default;
+				padding:10px 0;
+					.delete{
+						width: 2rem;
+						height: 2rem;
+						line-height: 2rem;
+						text-align: right;
+							i{
+								font-size: @size-default;
+							}
+					}
+
+			}
+		}
+		.nomore-history{
+			text-align: center;
+			font-size: 1.2rem;
+		}
+	}
+</style>
--- src/components/base/search/search.vue
+++ src/components/base/search/search.vue
@@ -5,6 +5,7 @@
 				<input class="searchBtn" type="submit">
 				<input type="text" class="search" placeholder="请输入"
                     id='focusInput'
+                    v-model = 'query'
 				>
 			</div>
 			<a slot='right' @click="$router.go(-1)">取消</a>
@@ -18,6 +19,7 @@
                             <h2>搜索历史</h2>
                             <i class="icon iconfont icon-lajitong"></i>
                         </div>
+                        <history-list></history-list>
                     </div>
                 </div>
             </scroll>
@@ -28,7 +30,15 @@
     import searchBox from '@/components/common/header'
     import scroll    from '@/components/base/scroll/scroll'
     export default{
-        components:{searchBox,scroll},
+        data(){
+            return {
+                query:'', //用户搜索的参数
+                throttle:false, //节流操作
+            }
+        },
+        components:{searchBox,scroll,
+            historyList:res=>require(['@/components/base/historyList/historyList.vue'],res)
+        },
         activated(){
             this.$nextTick(()=>{
                 this.focusInput();
@@ -40,55 +50,101 @@
                 let focusInput = document.getElementById('focusInput');
                 focusInput.focus();
 
+            },
+            onchangeQuery(newQuery){
+                console.log(1)
+                
+            }
+        },
+        watch:{
+            query(newQuery){
+
+                if(!this.throttle){//前端节流
+                    if(this.timer){
+                        clearTimeout(this.timer)
+                    
+                    }
+                    this.timer = setTimeout(()=>{
+                        this.onchangeQuery(newQuery);
+                    },200)
+                }
+               
+
             }
         }
     }
 </script>
 <style lang="less" scoped>
-div.search-warp{
-    height: 2.8rem;
-    background-color: #fff;
-    display: flex;
-    display: --webkit-flex;
-    border-radius: .4rem;
-    width: 90%;
-    margin-left: 1rem;
-    align-items: center;
-    justify-content: space-around;
-    .searchBtn{
-        border: none;
-        outline: none;
-        display: inline-block;
-        background: url('../../../assets/images/ic_search_gray.png')  no-repeat;
-        text-indent: -99px;
-        width: 20px;
-        height: 20px;
-        background-size: cover;
-    }
-    .search{
-        border:none;
-        outline: none;
-        width: 80%;
-        height: 90%;
-        font-size: 1rem;
-    }
-
-}
-.search-main{
-    position: fixed;
-    top:3.7rem;
-    width: 100%;
-    bottom: 0;
-    
-    padding: 0 15px;
-    overflow: hidden;
-
-        .search-history{
-                .search-describe{
-                    display: flex;
-                    width: 100%;
-                    justify-content: space-between;
-                }
+    @import '../../../assets/less/color.less';
+    div.search-warp{
+        height: 2.8rem;
+        background-color: #fff;
+        display: flex;
+        display: --webkit-flex;
+        border-radius: .4rem;
+        width: 90%;
+        margin-left: 1rem;
+        align-items: center;
+        justify-content: space-around;
+        .searchBtn{
+            border: none;
+            outline: none;
+            display: inline-block;
+            background: url('../../../assets/images/ic_search_gray.png')  no-repeat;
+            text-indent: -99px;
+            width: 20px;
+            height: 20px;
+            background-size: cover;
         }
-}
+        .search{
+            border:none;
+            outline: none;
+            width: 80%;
+            height: 90%;
+            font-size: 1rem;
+        }
+
+    }
+    .search-main{
+        position: fixed;
+        top:3.7rem;
+        width: 100%;
+        bottom: 0;
+        
+        padding: 0 15px;
+        overflow: hidden;
+
+            .search-history{
+                    .search-describe{
+                        display: flex;
+                        width: 100%;
+                    
+                        justify-content: space-between;
+                        padding:10px 0;
+                        margin-top: 1rem;
+                        position: relative;
+                            h2{
+                                color:@default-green-color;
+                                font-weight: normal;
+                                font-size: 1.4rem
+                            }
+                            i{
+                                color:@default-green-color;
+                                font-size: 1.4rem;
+                            }
+                            &:after{
+                                position: absolute;
+                                content: "";
+                                top:0;
+                                left: 0;
+                                right: 0;
+                                width: 100%;
+                                height: 1px;
+                                background-color: @line-default-color;
+                                transform:scaleY(.5);
+
+                            }
+                    }
+            }
+    }
 </style>
